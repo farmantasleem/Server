5b3006544a9ab19ce5ed73a5817d46d3af6bfbfb
farmanfirnas <iamfarmanmalik@gmail.com>
2022-09-28 10:12:11 +0530

    changes made

--- db.js
+++ db.js
@@ -2,12 +2,14 @@ const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
 const itemTypes = require("./data/itemTypes");
+const login = require("./data/login");
 
 module.exports = {
   items,
   tags,
   brands,
-  itemTypes
+  itemTypes,
+  login
 }
 
 
